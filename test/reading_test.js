0b2d0624ca276f9715cfd5fd08b1f7460f3494c0
prateek <prateeksingh8841@gmail.com>
2024-03-15 23:43:15 -0600

    feat: mongod

--- test/reading_test.js
+++ test/reading_test.js
@@ -7,9 +7,17 @@ describe("Reading users out of database", () => {
     joe = new User({ name: "Joe" });
     joe.save().then(() => done());
   });
+
   it("finds all users with name of joe", (done) => {
     User.find({ name: "Joe" }).then((users) => {
       assert(users[0]._id.toString() === joe._id.toString()); // ????
+      done();
+    });
+  });
+
+  it("find user with particular id", (done) => {
+    User.findOne({ _id: joe._id }).then((user) => {
+      assert(user.name === "Joe");
       done();
     });
   });
