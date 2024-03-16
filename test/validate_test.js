633614048a6f9b3df0e8fe0eb0280fd22eb55e69
prateek <prateeksingh8841@gmail.com>
2024-03-16 17:01:41 -0600

    feat: mongod

--- test/validate_test.js
+++ test/validate_test.js
@@ -1,8 +1,11 @@
-const assert = require('assert');
-const User = require('../src/user');
+const assert = require("assert");
+const User = require("../src/user");
 
-describe('Validating Records', () => {
-    it('requires a user name', (done) => {
-        done();
-    });
+describe("Validating Records", () => {
+  it("requires a user name", () => {
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name; // destructure
+    assert(message === 'Name is required.');
+  });
 });
