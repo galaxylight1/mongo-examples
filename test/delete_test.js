3c1cb733c4cb336b3e2fc8af3cdba314a94f71f3
prateek <prateeksingh8841@gmail.com>
2024-03-16 00:37:15 -0600

    feat: mongod

---
+++ test/delete_test.js
@@ -0,0 +1,23 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("deleting a user", () => {
+  let joe;
+
+  beforeEach((done) => {
+    joe = new User({ name: "Joe" });
+    joe.save().then(() => {
+      done();
+    });
+  });
+
+  it("model instance remove", (done) => {
+    joe
+      .deleteOne()
+      .then(() => User.findOne({ name: "Joe" }))
+      .then((user) => {
+        assert(user === null);
+        done();
+      });
+  });
+});
