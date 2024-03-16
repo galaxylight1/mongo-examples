4a7124e7953dc43be18db7bc84657d4f3a64f014
prateek <prateeksingh8841@gmail.com>
2024-03-16 16:18:23 -0600

    feat: mongod

--- src/user.js
+++ src/user.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: String
+  name: String,
+  postCount: Number,
 });
 
-const User = mongoose.model('user', UserSchema);
+const User = mongoose.model("user", UserSchema);
 
 module.exports = User;
