b3b2b8f340ec35c2b5d3692a66ff2742b77a7f57
prateek <prateeksingh8841@gmail.com>
2024-03-17 02:58:45 -0600

    feat: mongod

--- src/user.js
+++ src/user.js
@@ -6,9 +6,21 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   name: {
     type: String,
+    validate: {
+      validator: (name) => name.length > 2,
+      message: "Name must be longer than 2 characters.",
+    },
     required: [true, "Name is required."],
   },
   postCount: Number,
+  // posts: [PostSchema],
+  likes: Number,
+  blogPosts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "blogPost",
+    },
+  ],
 });
 
 const User = mongoose.model("user", UserSchema);
