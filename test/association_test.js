be6b48d9ac6e817471167eed903be72340bf4eb7
prateek <prateeksingh8841@gmail.com>
2024-03-17 20:43:02 -0600

    feat: mongod

--- test/association_test.js
+++ test/association_test.js
@@ -3,15 +3,19 @@ const User = require("../src/user");
 const Comment = require("../src/comment");
 const BlogPost = require("../src/blogPost");
 
-describe('Associations', () => {
-    let joe, blogPost, comment;
+describe("Associations", () => {
+  let joe, blogPost, comment;
 
-    beforeEach(() => {
-        joe = new User({ name: 'Joe' });
-        blogPost = new BlogPost({ title: 'JS is great', content: 'xyz'});
-        comment = new Comment({ content: 'Congrats' });
-        joe.blogPosts.push(blogPost); // associate b/w user and blog post
-        blogPost.comments.push(comment);
-        comment.user = joe;
+  beforeEach((done) => {
+    joe = new User({ name: "Joe" });
+    blogPost = new BlogPost({ title: "JS is great", content: "xyz" });
+    comment = new Comment({ content: "Congrats" });
+    joe.blogPosts.push(blogPost); // associate b/w user and blog post
+    blogPost.comments.push(comment);
+    comment.user = joe;
+
+    Promise.all([joe.save(), blogPost.save(), comment.save()]).then(() => {
+      done();
     });
+  });
 });
