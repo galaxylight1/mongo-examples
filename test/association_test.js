012a48a1d8d9f07d84c307315a5ddb27a0c63fe7
prateek <prateeksingh8841@gmail.com>
2024-03-17 20:39:42 -0600

    feat: mongod

--- test/association_test.js
+++ test/association_test.js
@@ -4,5 +4,14 @@ const Comment = require("../src/comment");
 const BlogPost = require("../src/blogPost");
 
 describe('Associations', () => {
-    
+    let joe, blogPost, comment;
+
+    beforeEach(() => {
+        joe = new User({ name: 'Joe' });
+        blogPost = new BlogPost({ title: 'JS is great', content: 'xyz'});
+        comment = new Comment({ content: 'Congrats' });
+        joe.blogPosts.push(blogPost); // associate b/w user and blog post
+        blogPost.comments.push(comment);
+        comment.user = joe;
+    });
 });
